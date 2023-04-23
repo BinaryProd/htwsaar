c32e500255fef897ce6b470a7bd32aac63d7a4b7
Ideflop <vi03pl@gmail.com>
2023-04-23 20:34:17 +0200

    Rename all file from gcd to ggt

--- 14ueb/ersteUebung/GgtException.java
+++ 14ueb/ersteUebung/GgtException.java
@@ -1,19 +1,19 @@
 /**
- * <h1> GcdException </h1>
+ * <h1> GgtException </h1>
  * 
  * @author Victor Plage and David Glaser
  * @version 1.0.
  * @since 2023-04-11
  */
-class GcdException extends RuntimeException {
+class GgtException extends RuntimeException {
 
     private static final String FEHLER_NUMMER = "Die Zahl muss groesser als 0 sein";
 
-    public GcdException() {
+    public GgtException() {
         super();
     }
 
-    public GcdException(String message) {
+    public GgtException(String message) {
         super(message);
     }
 
@@ -21,11 +21,11 @@ class GcdException extends RuntimeException {
      * Check if long is positiv or null
      *
      * @param long
-     * @throws GcdException
+     * @throws GgtException
      */
-    public static void checkIfValidNumber(long number) throws GcdException {
+    public static void checkIfValidNumber(long number) throws GgtException {
         if (number < 0) {
-            throw new GcdException(FEHLER_NUMMER);
+            throw new GgtException(FEHLER_NUMMER);
         }
     }
 }
