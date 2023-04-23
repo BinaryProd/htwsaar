50d3625bc77a0a1fb90d3858e1c1b66865002d0b
Ideflop <vi03pl@gmail.com>
2023-04-23 20:34:17 +0200

    Rename all file from gcd to ggt

--- 14ueb/ersteUebung/GgtDialog.java
+++ 14ueb/ersteUebung/GgtDialog.java
@@ -1,5 +1,5 @@
 /**
- * <h1> GcdDialog </h1>
+ * <h1> GgtDialog </h1>
  * 
  * @author Victor Plage and David Glaser
  * @version 1.0.
@@ -8,14 +8,14 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class GcdDialog {
+public class GgtDialog {
 
     // private fields for various menu options and their corresponding int and
     // String values
-    private static final int GCD = 1;
+    private static final int GGT = 1;
     private static final int EXIT = 10;
 
-    private static final String GCD_STRING = " : Gcd berechnen";
+    private static final String GGT_STRING = " : Ggt berechnen";
     private static final String EXIT_STRING = " : Das Programm beenden";
 
     private Scanner scanner = EingabeUtils.scanOpen();
@@ -24,7 +24,7 @@ public class GcdDialog {
      * The main function starts a new Dialog object.
      */
     public static void main(String[] args) {
-        new GcdDialog().start();
+        new GgtDialog().start();
     }
 
     /**
@@ -39,9 +39,11 @@ public class GcdDialog {
 
         while (userInput != EXIT) {
             try {
-                gcdMenuAusgabe();
+                ggtMenuAusgabe();
                 userInput = eingabeLesen();
-                gcdEingabeBearbeitung(userInput);
+                ggtEingabeBearbeitung(userInput);
+            } catch (GgtException e ){
+                System.out.println(e);
             } catch (IllegalArgumentException e) {
                 System.out.println(e);
             } catch (InputMismatchException e) {
@@ -61,10 +63,10 @@ public class GcdDialog {
      * Dialog. It uses a StringBuilder to append the different menu options and
      * their corresponding int and string values and prints them to the console.
      */
-    public void gcdMenuAusgabe() {
+    public void ggtMenuAusgabe() {
         StringBuilder sb = new StringBuilder();
 
-        sb.append("\n").append(GCD).append(GCD_STRING)
+        sb.append("\n").append(GGT).append(GGT_STRING)
             .append("\n").append(EXIT).append(EXIT_STRING).append("\n")
             .append(">>> ");
 
@@ -86,10 +88,10 @@ public class GcdDialog {
      * 
      * @param userInput The input from the user
      */
-    public void gcdEingabeBearbeitung(int userInput) {
+    public void ggtEingabeBearbeitung(int userInput) {
         switch (userInput) {
-            case GCD:
-                gcd();
+            case GGT:
+                ggt();
                 break;
             case EXIT:
                 System.out.println("Das Programm wird beendet");
@@ -102,9 +104,9 @@ public class GcdDialog {
     }
 
     /**
-     * This method give the gcd of the two number giving by the user
+     * This method give the ggt of the two number giving by the user
      */
-    public void gcd() {
+    public void ggt() {
         System.out.println("Erste zahl:");
         System.out.print(">>> ");
         long zahl1 = EingabeUtils.scanLong(scanner);
@@ -113,7 +115,7 @@ public class GcdDialog {
         System.out.print(">>> ");
         long zahl2 = EingabeUtils.scanLong(scanner);
 
-        System.out.println(Gcd.gcd(zahl1, zahl2));
+        System.out.println(Ggt.ggt(zahl1, zahl2));
     }
 
 }
---
+++ 14ueb/ersteUebung/GgtTest.java
@@ -0,0 +1,58 @@
+/**
+ * <h1> GgtTest </h1>
+ * 
+ * @author Victor Plage and David Glaser
+ * @version 1.0.
+ * @since 2023-04-11
+ */
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GgtTest {
+
+    @Test
+    public void testGgt() {
+        long result = Ggt.ggt(10, 25);
+        assertEquals(5, result);
+    }
+    
+    @Test
+    public void testGgtWithZero() {
+        long result = Ggt.ggt(25, 0);
+        assertEquals(0, result);
+        
+        long result2 = Ggt.ggt(0, 25);
+        assertEquals(0, result2);
+    }
+    
+    @Test
+    public void testGgtWithNegativeNumbers()  {
+        assertThrows(GgtException.class, () -> Ggt.ggt(-10, 25));
+        
+        assertThrows(GgtException.class, () -> Ggt.ggt(10, -25));
+
+        assertThrows(GgtException.class, () -> Ggt.ggt(-10, -10));
+    }
+    
+    @Test
+    public void testGgtWithSameNumbers()  {
+        long result = Ggt.ggt(10, 10);
+        assertEquals(10, result);
+    }
+    
+    @Test
+    public void testGgtWithOneNumber()  {
+        long result1 = Ggt.ggt(10, 1);
+        assertEquals(1, result1);
+        
+        long result2 = Ggt.ggt(1, 10);
+        assertEquals(1, result2);
+    }
+    
+    @Test
+    public void testGgtWithLargeNumbers()  {
+        long result = Ggt.ggt(1000000000, 500000000);
+        assertEquals(500000000, result);
+    }
+}
+
