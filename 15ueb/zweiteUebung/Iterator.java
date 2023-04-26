26b4a7a989446ec119bef01d331abdd53ee0a90d
Ideflop <vi03pl@gmail.com>
2023-04-26 20:47:11 +0200

    iterator

---
+++ 15ueb/zweiteUebung/Iterator.java
@@ -0,0 +1,29 @@
+public class Iterator {
+    private int currentIndex;
+    private int arrayLength;
+
+    public Iterator(int arrayLength) {
+        this.arrayLength = arrayLength;
+        currentIndex = 0;
+    }
+
+    public boolean hasNext() {
+        return currentIndex++ < arrayLength();
+    }
+
+    public int getCurrentIndex() {
+        return currentIndex;
+    }
+
+    public int getArrayLength() {
+        return arrayLength;
+    }
+
+    public void setCurrentIndex(int new_value) {
+        this.currentIndex = new_value;
+    }
+
+    public void getArrayLength(int new_value) {
+        this.arrayLength = new_value
+    }
+}
