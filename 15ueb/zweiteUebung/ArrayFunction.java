8bdbd748680786eaf6cdcfcca2ce4f8c9f11118f
0m3re <d2ave@gmx.de>
2023-04-26 22:14:28 +0200

    TopLevel Class

---
+++ 15ueb/zweiteUebung/ArrayFunction.java
@@ -0,0 +1,103 @@
+public class ArrayFunction {
+    
+    private Object[] array;    
+    private int size;    
+    
+    public ArrayFunction(int capacity) {    
+        array = new Object[capacity];    
+        size = 0;    
+    }    
+    
+    public void addFirst(Object value) {    
+        if (size == array.length) {    
+            System.err.println("Error: Array is full.");    
+            return;    
+        }    
+        for (int i = size - 1; i >= 0; i--) {    
+            array[i + 1] = array[i];    
+        }    
+        array[0] = value;    
+        size++;    
+    }    
+    
+    public void removeFirst() {    
+        if (size == 0) {    
+            System.err.println("Error: Array is empty.");    
+            return;    
+        }    
+        for (int i = 1; i < size; i++) {    
+            array[i - 1] = array[i];    
+        }    
+        array[size - 1] = null;    
+        size--;    
+    }    
+    
+    public void addEnd(Object value) {    
+        if (size == array.length) {    
+            System.err.println("Error: Array is full.");    
+            return;    
+        }    
+        array[size] = value;    
+        size++;    
+    }    
+    
+    public void removeEnd() {    
+        if (size == 0) {    
+            System.err.println("Error: Array is empty.");    
+            return;    
+        }    
+        array[size - 1] = null;    
+        size--;    
+    }    
+    
+    public void addAtIndex(int index, Object value) {    
+        if (size == array.length) {    
+            System.err.println("Error: Array is full.");    
+            return;    
+        }    
+        if (index < 0 || index > size) {    
+            System.err.println("Error: Invalid index.");    
+            return;    
+        }    
+        for (int i = size - 1; i >= index; i--) {    
+            array[i + 1] = array[i];    
+        }    
+        array[index] = value;    
+        size++;    
+    }    
+    
+    public void removeAtIndex(int index) {    
+        if (size == 0) {    
+            System.err.println("Error: Array is empty.");    
+            return;                                                                                                                                                                                                                                         
+        }                                                                                                                                                                                                                                                   
+        if (index < 0 || index >= size) {                                                                                                                                                                                                                   
+            System.err.println("Error: Invalid index.");                                                                                                                                                                                                    
+            return;                                                                                                                                                                                                                                         
+        }                                                                                                                                                                                                                                                   
+        for (int i = index; i < size - 1; i++) {                                                                                                                                                                                                            
+            array[i] = array[i + 1];                                                                                                                                                                                                                        
+        }                                                                                                                                                                                                                                                   
+        array[size - 1] = null;                                                                                                                                                                                                                                
+        size--;                                                                                                                                                                                                                                             
+    }                                                                                                                                                                                                                                                       
+
+    public int getSize() {
+        return size;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            System.err.println("Error: Invalid index.");
+            return -1;  // or any other default value
+        }
+        return array[index];
+    }
+
+
+    public Object[] getArray() {                                                                                                                                                                                                         
+        Object[] copyArray = new Object[size];
+        System.arraycopy(array, 0, copyArray, 0, size);
+        return copyArray;
+    }
+}  
--- 15ueb/zweiteUebung/NumberCruncherTopLevel.java
+++ 15ueb/zweiteUebung/NumberCruncherTopLevel.java
@@ -1,5 +1,6 @@
 public class NumberCruncherTopLevel implements NumberCruncher {
     private float[] numbers;
+    private itarator = new Iterator(numbers.length);
 
     public NumberCruncherTopLevel(float[] numbers) {
         this.numbers = numbers;
@@ -7,13 +8,86 @@ public class NumberCruncherTopLevel implements NumberCruncher {
 
     public void crunch(String[] operations) {
         for (operation : operations) {
-            if (operation.equals("sum")) {
+            switch(operation) {
+                case "sum":
+                    while (iterator.hasNext()) {
+                        int i = iterator.getCurrentIndex();
+                        numbers[i+1] = numbers[i] + numbers[i+1];
+                        iterator.updateIndex();
+                    }
+                    break;
+                case "swirl":
+                    Random random = new Random();
+                    while (iterator.hasNext()) {
+                        int j = random.nextInt(numbers.length);
+                        int k = random.nextInt(numbers.length);
+                        float temp = numbers[k];
+                        numbers[k] = numbers[j];
+                        numbers[j] = temp;
+                        iterator.updateIndex();
+                    }
+                    break;
+                case "divide":
+                    while (iterator.hasNext()) {
+                        ArrayFunction temp = new ArrayFunction(numbers.length);
+                        ArrayFunction indexes = new ArrayFunction(numbers.length);
+                        int i = iterator.getCurrentIndex();
+                        int testNumber = 0;
+
+                        boolean added = false; 
+
+                        for (int j = 0; j < temp.getSize(); j++) {
+                            if (numbers[i] < temp.get(j)) {
+                                temp.addAtIndex(j, numbers[i]);
+                                indexes.addAtIndex(j, i);
+                                added = true;
+                                break;
+                            }
+                        }
+
+                        if (!added) {
+                            temp.addEnd(numbers[i]);
+                            indexes.addEnd(i);
+                        }
+
+                        iterator.updateIndex();
+                    }
+                    int j = 0;
+                    for(int i = numbers.length; i > numbers.length/2; i--) { // attention if numbers.length is odd
+                        numbers[indexes.get(i)] = temp.get(i) / temp.get(j);
+                        j++;
+                    }
+
+                    break;
+                case "subtract":
+                    while (iterator.hasNext()) {
+                        int i = iterator.getCurrentIndex();
+                        numbers[i+1] = numbers[i] - numbers[i+1];
+                        iterator.updateIndex();
+                    }
+                    break;
+                case "average":
+                    float avg = 0;
+                    float biggestNumber = 0;
+                    int index = 0;
+                    while (iterator.hasNext()) {
+                        int i = iterator.getCurrentIndex();
+                        avg += numbers[i];
+                        if (numbers[i] > biggestNumber) {
+                            biggestNumber = numbers[i];
+                            index = i;
+                        }
+                        iterator.updateIndex();
+                    }
+                    numbers[index] = avg / numbers.length;
+                    break;
+
             }
         }
-        }
     }
+}
 
-    public float[] getNumbers() {
-        return numbers;
-    }
+public float[] getNumbers() {
+    return numbers;
+}
 }
