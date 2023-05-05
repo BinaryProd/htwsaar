510e9f2220b97f48f0aa443aebdf850cc26ff49f
Ideflop <vi03pl@gmail.com>
2023-05-05 18:27:15 +0200

    automat exception

--- 16ueb/Getraenkeautomat.java
+++ 16ueb/Getraenkeautomat.java
@@ -4,20 +4,31 @@ public class Getraenkeautomat < T extends Getraenk> {
     private ArrayList<Flasche<T>> flaschenlager;
     private int kapazitaet;
 
-    public Getraenkeautomat(int kapazitaet) {
-        // check kapazitaet
+    public Getraenkeautomat(int kapazitaet) throws GetraenkeautomatException {
+        if (kapazitaet <= 0) {
+            throw new GetraenkeautomatException("Kapazitaet muss groesser 0 sein");
+        }
+
         this.kapazitaet = kapazitaet;
         this.flaschenlager = new ArrayList<Flasche<T>>();
     }
 
-    public void flascheEinlegen(Flasche<T> flasche) {
-        // check if kapazitaet voll
-        // check if flashce gefiuellt flasche.istGefuellt()
+    public void flascheEinlegen(Flasche<T> flasche) throws GetraenkeautomatException {
+        if (this.flaschenlager.size() >= this.kapazitaet) {
+            throw new GetraenkeautomatException("Kapazitaet ist voll");
+        }
+        if (!flasche.istGefuellt()) {
+            throw new GetraenkeautomatException("Flasche ist nicht gefuellt");
+        }
+
         this.flaschenlager.add(flasche);
     }
 
-    public Flasche<T> flascheAusgeben() {
-        // test if empty else eroor => is null or size 0
+    public Flasche<T> flascheAusgeben() throws GetraenkeautomatException {
+        if (this.flaschenlager.size() == 0) {
+            throw new GetraenkeautomatException("Keine Flaschen mehr vorhanden");
+        }
+
         return this.flaschenlager.remove(0);
     }
 
---
+++ 16ueb/GetraenkeautomatException.java
@@ -0,0 +1,11 @@
+public class GetraenkeautomatException extends RuntimeException {
+
+    public GetraenkeautomatException() {
+        super();
+    }
+
+    public GetraenkeautomatException(String message) {
+        super(message);
+    }
+
+}
