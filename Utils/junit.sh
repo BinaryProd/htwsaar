164601d1a13b93ae744dcbe260c3816b2547542e
Ideflop <vi03pl@gmail.com>
2023-05-19 09:46:05 +0200

    junit 10

--- Utils/junit.sh
+++ Utils/junit.sh
@@ -1,9 +1,9 @@
 test_files=$(find . -name "*.java")
 
 if [ "$USER" = "om3re" ]; then
-    java -jar /home/om3re/.config/junit/junit-platform-console-standalone-1.9.2.jar --class-path . --scan-class-path
+    java -jar /home/om3re/.config/junit/junit-platform-console-standalone-1.10.0-M1.jar --class-path . --scan-class-path
     exit 0
 elif [ "$USER" = "idefux" ]; then
-    java -jar /home/idefux/Dokumente/school/l2/semester2/programmierung2/exercice/Utils/junit/junit-platform-console-standalone-1.9.2.jar --class-path . --scan-class-path
+    java -jar /home/idefux/Dokumente/school/l2/semester2/programmierung2/exercice/Utils/junit/junit-platform-console-standalone-1.10.0-M1.jar --class-path . --scan-class-path
     exit 0
 fi
