43de38bcf7408b9275e471de2c9fa64d9f8f6c2a
Ideflop <vi03pl@gmail.com>
2023-05-19 10:19:24 +0200

    null instead of x

--- 17ueb/Function.java
+++ 17ueb/Function.java
@@ -90,7 +90,7 @@ public class Function {
 
     public IntPredicate even = x -> x % 2 == 0;
 
-    public MyFunction lambdaEvenSquare = x -> even.test(x) ? lambdaSquare.apply(x) : x;
+    public MyFunction lambdaEvenSquare = x -> even.test(x) ? lambdaSquare.apply(x) : null;
 
     public MyFunction lambdaOddFactoriel = x -> (odd.test(lambdaFactoriel.apply(x))) ? lambdaFactoriel.apply(x) : 0;
 
