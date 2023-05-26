07ef2a9e3c37d1f00cd647d9c8957581d134c427
Ideflop <94184575+Ideflop@users.noreply.github.com>
2023-05-26 13:21:37 +0200

    fix bug pre-commit check

--- Utils/compile.sh
+++ Utils/compile.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Find the latest directory
-latest_dir=$(ls -td -- */ | head -n 1)
+#latest_dir=$(ls -td -- */ | head -n 1)
 
 # Compile Java files in the latest directory
-javac -cp .:Utils/junit-platform-console-standalone-1.10.0-M1.jar "${latest_dir}"*.java
+#javac -cp .:Utils/junit-platform-console-standalone-1.10.0-M1.jar "${latest_dir}"*.java
+javac -cp .:Utils/junit-platform-console-standalone-1.10.0-M1.jar *.java
 
