d3e361dcfdb3301e02f294fd9a6cb9a8d608c115
0m3re <d2ave@gmx.de>
2023-06-15 22:10:00 +0200

    flemmeDeFairePlus

---
+++ 20ueb/aufgabe3/MinHeap.java
@@ -0,0 +1,57 @@
+import java.util.Collection;
+import java.util.Queue;
+import java.lang.UnsupportedOperationException;
+
+public class MinHeap<E> implements Queue<E> {
+    public boolean offer(E e){
+        // TODO
+    }
+
+    public E poll(){
+        // TODO
+    }
+
+    public E peek(){
+        // TODO
+    }
+
+    public Collection<E> size(){
+        throw new UnsupportedOperationException();
+    }
+
+    public Queue<E> add(E e){
+        throw new UnsupportedOperationException();
+    }
+    
+    public Collection<E> remove(Object o){
+        throw new UnsupportedOperationException();
+    }
+
+    public Queue<E> remove(){
+        throw new UnsupportedOperationException();
+    }
+
+    public Collection<E> addAll(Collection<? extends E> c){
+        throw new UnsupportedOperationException();
+    }
+
+    public Collection<E> isEmpty(){
+        throw new UnsupportedOperationException();
+    }
+
+    public Collection<E> containsAll(Collection<?> c){
+        throw new UnsupportedOperationException();
+    }
+
+    public Collection<E> removeAll(Collection<?> c){
+        throw new UnsupportedOperationException();
+    }
+
+    public Collection<E> toArray(){
+        throw new UnsupportedOperationException();
+    }
+
+    public Collection<E> toArray(T[] a){
+        throw new UnsupportedOperationException();
+    }
+}
