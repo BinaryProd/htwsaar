228d81878447e7a2cae6cf2da1e4c5269bd47be9
Ideflop <94184575+Ideflop@users.noreply.github.com>
2023-06-20 19:05:45 +0200

    aufgabe 1: remove static in test import

--- 20ueb/aufgabe1/QuersummeTest.java
+++ 20ueb/aufgabe1/QuersummeTest.java
@@ -1,9 +1,9 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class QuersummeTest {
 
