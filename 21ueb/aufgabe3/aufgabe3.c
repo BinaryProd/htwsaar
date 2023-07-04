611d4a1daabd0a6acc517600e339d9545791a19e
Ideflop <vi03pl@gmail.com>
2023-07-04 12:44:57 +0200

    a

--- 21ueb/aufgabe3/aufgabe3.c
+++ 21ueb/aufgabe3/aufgabe3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 int validateInput() {
     int number;
@@ -6,8 +7,8 @@ int validateInput() {
     printf("Geben Sie einen Wert ein: ");
     scanf("%d", &number);
 
-    if (number < 0) {
-        printf("Die Zahl sollte positiv sein.\n");
+    if (number <= 0) {
+        printf("Die Zahl soll größer als 0 sein!\n");
         return -1;
     }
 
@@ -15,14 +16,21 @@ int validateInput() {
 }
 
 void printFibonacci(int number) {
-    int prev = 0;
+    int prev = 1;
     int current = 1;
     int next;
 
     printf("Fibonacci-Zahlen:\n");
 
     for (int i = 0; i < number; i++) {
-        printf("%d\n", prev);
+        printf("%d\n", current);
+
+        // Check for potential overflow before performing addition
+        if (current > INT_MAX - prev) {
+            printf("Integer overflow detected!\n");
+            break;
+        }
+
         next = prev + current;
         prev = current;
         current = next;
@@ -40,3 +48,4 @@ int main() {
 
     return 0;
 }
+
